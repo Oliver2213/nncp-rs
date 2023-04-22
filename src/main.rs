1dbde86d2a62e43a89b04c9885b320ab56b290b7
Blake Oliver <oliver22213@me.com>
2023-04-22 10:02:28 -0400

    Get 2 out of the 3 keys for an nncp node generating
    Right now of course we're just generating the keypairs and not the json, or modifying any configs. But its a start!

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,19 @@ use ed25519_compact::Seed;
 
 // first, let's make an ed25519 keypair.
 fn main() {
-    let b32_alph = RFC4648 { padding: true };
+    let b32_alph = RFC4648 { padding: false };
     println!("Generating ed25519 keypair...");
     let key_pair = KeyPair::from_seed(Seed::generate());
-    let enc_keypair_pub = encode(b32_alph, &key_pair.pk.as_ref());
-    println!("Encoded public key: {enc_keypair_pub:?}");
-    println!("{key_pair:?}");
+    let encoded_ed_pub = encode(b32_alph, &key_pair.pk.as_ref());
+    let encoded_ed_prv = encode(b32_alph, &key_pair.sk.as_ref());
+    println!("Encoded public key: {encoded_ed_pub:?}");
+    println!("Encoded ed private key: {encoded_ed_prv:?}");
     println!("Creating nacl box keypair (exchpub and exchprv)...");
     let nacl_secret_key = SecretKey::generate(&mut OsRng);
-    println!("Created private key {nacl_secret_key:?}");
+    let nacl_prv_bytes = nacl_secret_key.as_bytes();
     let nacl_pubkey_bytes = nacl_secret_key.public_key().as_bytes().clone();
+    let encoded_nacl_pub = encode(b32_alph, &nacl_pubkey_bytes);
+    let encoded_nacl_prv = encode(b32_alph, nacl_prv_bytes);
+    println!("exchpub: {encoded_nacl_pub}");
+    println!("excprv: {encoded_nacl_prv}");
 }
