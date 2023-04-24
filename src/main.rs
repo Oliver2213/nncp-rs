5e95c3e4a2fb53f247e3b87941e4f3f8023e397f
Blake Oliver <oliver22213@me.com>
2023-04-24 05:32:55 -0400

    Add build_info dependency, for including build time, rustc version in output; later on mention current branch, commit and dirty state with --version; clean up main() a bit, using keys from the generated node

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ pub struct LocalNNCPNode {
     pub id: [u8; 32],
     /// exchange private key
     pub exchprv: crypto_box::SecretKey,
-    /// 
+    /// Signing key (node ID derived from hash of public key)
     pub signing_kp: ed25519_compact::KeyPair,
     /// Noise protocol keypair, used for nncp sync protocol
     pub noise_kp: snow::Keypair,
@@ -46,34 +46,23 @@ impl LocalNNCPNode {
 
 }
 
-// first, let's make an ed25519 keypair.
 fn main() {
     let node: LocalNNCPNode = LocalNNCPNode::generate();
     let b32_alph = RFC4648 { padding: false };
+    let build_info = build_info::format!("Built at {} with {}", $.timestamp, $.compiler);
+    println!("{build_info}");
+    let encoded_node_id = encode(b32_alph, node.id.as_ref());
+    println!("Node ID: {encoded_node_id}");
     let encoded_ed_pub = encode(b32_alph, &node.signing_kp.pk.as_ref());
     let encoded_ed_prv = encode(b32_alph, &node.signing_kp.sk.as_ref());
     println!("Encoded public key: {encoded_ed_pub:?}");
     println!("Encoded ed private key: {encoded_ed_prv:?}");
-    println!("Creating nacl box keypair (exchpub and exchprv)...");
-    let nacl_secret_key = SecretKey::generate(&mut OsRng);
-    let nacl_prv_bytes = nacl_secret_key.as_bytes();
-    let nacl_pubkey_bytes = nacl_secret_key.public_key().as_bytes().clone();
-    let encoded_nacl_pub = encode(b32_alph, &nacl_pubkey_bytes);
-    let encoded_nacl_prv = encode(b32_alph, nacl_prv_bytes);
+    let encoded_nacl_pub = encode(b32_alph, &node.exchprv.public_key().as_bytes().clone());
+    let encoded_nacl_prv = encode(b32_alph, &node.exchprv.as_bytes().clone());
     println!("exchpub: {encoded_nacl_pub}");
     println!("exchprv: {encoded_nacl_prv}");
-    let nb: snow::Builder = Builder::new(PATTERN.parse().unwrap());
-    let noise_keypair = nb.generate_keypair().unwrap();
-
-    // Now we encode .public and .private into base32 like above
-    let encoded_noise_pub = encode(b32_alph, &noise_keypair.public);
-    let encoded_noise_prv = encode(b32_alph, &noise_keypair.private);
+    let encoded_noise_pub = encode(b32_alph, &node.noise_kp.public);
+    let encoded_noise_prv = encode(b32_alph, &node.noise_kp.private);
     println!("noisepub: {encoded_noise_pub}");
     println!("noiseprv: {encoded_noise_prv}");
-    // Node ID is blake2s256 hash of the signing public key (ed25519)
-    let mut hasher: Blake2s256 = Blake2s256::new();
-    hasher.update(&sign_keypair.pk.as_ref());
-    let node_id = hasher.finalize();
-    let encoded_node_id = encode(b32_alph, node_id.as_ref());
-    println!("Node ID: {encoded_node_id}");
 }
