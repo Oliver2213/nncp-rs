d83e407174a18213b2481538fa02200118227d04
Blake Oliver <oliver22213@me.com>
2023-04-30 19:07:19 -0400

    Fix a lint in constants; add clap as a dependency for argument parsing

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
 //! various nncp constants
 
 /// the noise pattern used by nncp
-pub static NOISE_PROTO_PATTERN: &'static str = "Noise_IK_25519_ChaChaPoly_BLAKE2b";
+pub static NOISE_PROTO_PATTERN: &str = "Noise_IK_25519_ChaChaPoly_BLAKE2b";
