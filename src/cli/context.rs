8e4adb4eee5fc8cc84d9af09d564e5aff107656c
Blake Oliver <oliver22213@me.com>
2023-05-12 04:01:23 -0400

    Improve error messages and add more cases in context; begin work on set_neighbors, for parsing neighbor node disk configs and setting them up on the context

--- src/cli/context.rs
+++ src/cli/context.rs
@@ -1,14 +1,16 @@
 //! Context related to a command invocation
 
+use crate::cli::config::RemoteNodeDiskConfig;
+
 use super::config::{DiskConfig, LocalNodeDiskConfig};
 use anyhow::Context as anyhow_context;
 use anyhow::{anyhow, Error};
 use base32::{decode, Alphabet::RFC4648};
 use log::{debug, error, info, trace, warn};
 use nncp_rs::nncp::{LocalNNCPNode, NodeID, RemoteNNCPNode};
+use std::collections::HashMap;
 use std::path::Path;
 use std::path::PathBuf;
-use std::collections::HashMap;
 
 /// Related context for nncp operations: config, spool, node keys, oh my!
 /// Gets passed to every command function.
@@ -80,7 +82,7 @@ impl Context {
         debug!("Config path: {}", &self.config_path.display());
         let config: DiskConfig =
             confy::load_path(&self.config_path).context("couldn't load nncp configuration")?;
-        self.set_local_node(&config.localnode);
+        self.set_local_node(&config.localnode)?;
         trace!("Created and stored local node on context");
         self.config = Some(config);
         debug!("Set up context");
@@ -118,36 +120,90 @@ impl Context {
         }
         let _exchpub: [u8; 32] = match exchpub_b32.unwrap().try_into() {
             Ok(b) => b,
-            Err(e) => return Err(anyhow!("Exchange secret key was incorrect size")),
+            Err(e) => {
+                error!("Exchange public key was incorrect size (must be 32 bytes)");
+                return Err(anyhow!(
+                    "The local node's exchange public key has incorrect size (must be 32 bytes)"
+                ));
+            }
         };
         trace!("Parsed exchpub into bytes");
         let exchpriv_bytes: [u8; 32] = match exchpriv_b32.unwrap().try_into() {
             Ok(p) => p,
-            Err(e) => return Err(anyhow!("exchange secret key was incorrect size")),
+            Err(e) => {
+                error!("Exchange secret key was incorrect size (must be 32 bytes)");
+                return Err(anyhow!(
+                    "The local node's exchange secret key has incorrect size (must be 32 bytes)"
+                ));
+            }
         };
         trace!("Parsed exchpriv into bytes");
         let noisepub_b32 = decode(b32_alph, &node.noisepub);
         let noisepriv_b32 = decode(b32_alph, &node.noiseprv);
         if noisepub_b32.is_none() || noisepriv_b32.is_none() {
-            error!("Unable to parse noise protocol keys as base32");
-            return Err(anyhow!("Unable to parse noise protocol keys as base32"));
+            error!("Unable to parse local node's noise protocol keys as base32");
+            return Err(anyhow!("Unable to parse local node's noise protocol keys as base32"));
+        }
+        let noisepub = noisepub_b32.unwrap();
+        if noisepub.len() != 32 {
+            error!("Noise public key isn't 32 bytes");
+            return Err(anyhow!("Local node's noise public key isn't 32 bytes"));
+        }
+        let noiseprv = noisepriv_b32.unwrap();
+        if noiseprv.len() != 32 {
+            error!("Local node noise private key isn't 32 bytes");
+            return Err(anyhow!("Local node's noise private key isn't 32 bytes"));
         }
         let our_node = LocalNNCPNode::new(
             signpriv_bytes,
             exchpriv_bytes,
-            noisepriv_b32.unwrap(),
-            noisepub_b32.unwrap(),
+            noiseprv,
+            noisepub,
         )?;
         self.local_node = Some(our_node);
         Ok(())
     }
-    
+
     /// Given a number of bytes, return if the disk our spool directory is mounted on has that much free space
     pub fn enough_spool_space(&self, size: u64) -> Result<bool, Error> {
         // Determine how much space is free on spool-holding-disk
         let available = fs2::available_space(&self.spool_path)?;
         Ok(available <= size)
-    
-    
+    }
+
+    pub fn set_neighbors(
+        &mut self,
+        neighbors_config: HashMap<String, RemoteNodeDiskConfig>,
+    ) -> Result<(), Error> {
+        // for each (friendly name, remote node) in the map, parse the node's keys, create an instance and save it under context.neighbors[id]
+        // also add an alias pointing from friendly name to ID once added.
+        // Later: via parsing, making sure that if a neighbor says it routes via another, we have those nodes information
+        // Ugh, right now this is going to be ugly. Later, abstract this decoding and converting to runtime struct into some method of each that just accepts the disk config versions or something
+        let b32_alph = RFC4648 { padding: false };
+        for (name, node) in &neighbors_config {
+            let signpub_bytes = decode(b32_alph, &node.signpub);
+            let exchpub_bytes = decode(b32_alph, &node.exchpub);
+            if signpub_bytes.is_none() {
+                error!(
+                    "Failed to base32 decode signing public key for node '{}'",
+                    &name
+                );
+                return Err(anyhow!(
+                    "Unable to parse signing key for node '{}' as valid base32",
+                    &name
+                ));
+            }
+            if exchpub_bytes.is_none() {
+                error!(
+                    "Failed to base32 decode exchange public key for node '{}'",
+                    &name
+                );
+                return Err(anyhow!(
+                    "Unable to parse exchange key for node '{}' as valid base32",
+                    &name
+                ));
+            }
+        }
+        todo!();
     }
 }
